b9428c4dc025bbbf6a1d7a7253df2dabb113c9b7
Etgar Avshalomov <EtgarAvshalomov@outlook.com>
2024-03-30 00:32:10 +0300

    Incredible!

--- Media App/Main.cpp
+++ Media App/Main.cpp
@@ -14,7 +14,7 @@ using namespace std;
 
 int main()
 {
-	//PlaySound(TEXT("RiveR - Solo"), NULL, SND_FILENAME | SND_LOOP | SND_ASYNC); // Fitgirl //
+	PlaySound(TEXT("RiveR - Solo"), NULL, SND_FILENAME | SND_LOOP | SND_ASYNC); // Fitgirl //
 
 	Viewer viewer(1, 0, 0, 0, "", "");
 	Manager manager(1, 0, 0, 0, "", "");
@@ -46,12 +46,12 @@ int main()
 			break;
 
 		case 1:
-			//cout << CYAN;
+			cout << CYAN;
 			viewer.ViewerMenu();
 			break;
 
 		case 2:
-			//cout << RED;
+			cout << RED;
 			manager.ManagerMenu();
 			break;
 
